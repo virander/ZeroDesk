770eabae9c56163f708245590565e1a63e10a88f
Apple <apple@java.local>
2017-12-12 16:28:40 +0530

    first commit

---
+++ Framework/AESKit.framework/Headers/AESKit.h
@@ -0,0 +1,19 @@
+//
+//  AESKit.h
+//  AESKit
+//
+//  Created by Apple  on 12/12/17.
+//  Copyright © 2017 Apple . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AESKit.
+FOUNDATION_EXPORT double AESKitVersionNumber;
+
+//! Project version string for AESKit.
+FOUNDATION_EXPORT const unsigned char AESKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AESKit/PublicHeader.h>
+
+
